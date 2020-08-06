deaac5f6aef6fef917212cfc9fae54bdec07d6f1
Mike Cooper <cooperm27@gmail.com>
2020-08-06 11:30:53 -0400

    changed again

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -4,8 +4,12 @@ public class MidTerm {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Twice upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
+<<<<<<< Updated upstream
 System.out.println("They were very clever monkeys indeed, known for their ingenious teamwork.");
 System.out.println("hello");
+=======
+System.out.println("They were very clever dogs indeed, known for their ingenious teamwork.");
+>>>>>>> Stashed changes
 	}
 
 }
