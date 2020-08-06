47f2e3c17f203bebfb5c0c0afd515e11a7940f10
Mike Cooper <cooperm27@gmail.com>
2020-08-06 14:04:24 -0400

    trying again cashPayment

--- src/CashPayment.java
+++ src/CashPayment.java
@@ -1,4 +1,27 @@
+import java.util.Scanner;
 
-public class CashPayment {
+public class CashPayment extends Payment {
+
+	private static Scanner scnr = new Scanner(System.in);	
+	
+	public CashPayment(double amount) {
+		super(amount);
+	}
+
+	@Override
+	public String Payment(double amount) {
+		System.out.println("How much cash are you giving us?");
+		double cashAmount = scnr.nextDouble();
+		double change = cashAmount - amount;
+		String outPut = "Thank you.  Your change is $" + change + "($" + cashAmount + "- $" +  amount + ")";
+		return outPut;
+	}
+
+	@Override
+	public String toString() {
+		return "CashPayment [toString()=" + super.toString() + "]";
+	}
+	
+	
 
 }
